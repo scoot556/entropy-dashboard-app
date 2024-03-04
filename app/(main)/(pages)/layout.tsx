d73cdb8ca78e16aa2e51d45853adbbc0c95a4bfe
Scott Djuric <scottdjuric@hotmail.com>
2024-03-04 21:51:24 +1100

    added a reset button for the news section as well as technology

--- app/(main)/(pages)/layout.tsx
+++ app/(main)/(pages)/layout.tsx
@@ -1,6 +1,6 @@
 const PagesLayout = ({children}: {children: React.ReactNode;}) => {
     return (
-        <div className="p-10 flex-1">
+        <div className="p-4 flex-1">
             {children}
         </div>
     )
