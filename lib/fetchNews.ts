c4aaa318449b532bbd5cfd3f591047a7dd0be864
Scott Djuric <scottdjuric@hotmail.com>
2024-03-04 16:38:48 +1100

    updated env variables

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,7 +1,7 @@
 export async function fetchNews() {
-    return fetch(`https://newsapi.org/v2/top-headlines?country=au&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`).then((response) => response.json());
+    return fetch(`https://newsapi.org/v2/top-headlines?country=au&apiKey=${process.env.NEWS_API_KEY}`).then((response) => response.json());
 }
 
 export async function fetchNewsByCategory(category: string) {
-    return fetch(`https://newsapi.org/v2/top-headlines?country=au&category=${category}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`).then((response) => response.json());
+    return fetch(`https://newsapi.org/v2/top-headlines?country=au&category=${category}&apiKey=${process.env.NEWS_API_KEY}`).then((response) => response.json());
 }
--- lib/fetchWeather.ts
+++ lib/fetchWeather.ts
@@ -1,9 +1,9 @@
 export async function fetchWeather(location: string | number[] | undefined) {
     if (typeof location === "string") {
-        return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`)
+        return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${process.env.OPEN_WEATHER_API_KEY}`)
             .then((response) => response.json()).then((result) => {
                 if (result.length === 1) {
-                    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${result[0].lat}&lon=${result[0].lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&units=metric`)
+                    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${result[0].lat}&lon=${result[0].lon}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`)
                         .then((response) => response.json());
                 } else {
                     const filteredResults = result.filter((item:any, index:any, array: any) => 
@@ -19,10 +19,10 @@ export async function fetchWeather(location: string | number[] | undefined) {
     }
 
     if (typeof location === "number") {
-        return fetch(`https://api.openweathermap.org/data/2.5/weather?id=${location}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&units=metric`).then((response) => response.json());
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?id=${location}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`).then((response) => response.json());
     }
 
     if (Array.isArray(location)) {
-        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location[0]}&lon=${location[1]}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&units=metric`).then((response) => response.json());
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location[0]}&lon=${location[1]}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`).then((response) => response.json());
     }
 }
